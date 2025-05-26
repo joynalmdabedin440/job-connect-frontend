5318979550c1618c4beb0ce232d110e53b5902a9
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-05-27 00:21:02 +0600

    add password validation regular expressiion

--- src/pages/auth/register/Register.jsx
+++ src/pages/auth/register/Register.jsx
@@ -3,6 +3,29 @@ import Lottie from 'lottie-react';
 import lottieAnimation from "../../../assets/lottie/register.json"
 
 const Register = () => {
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/;
+
+        if (!passwordRegex.test(password)) {
+            alert("Password must be exactly 6 characters long and include both letters and numbers.")
+
+
+            return
+            
+        }
+
+
+
+
+        
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,14 +34,14 @@ const Register = () => {
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <div className="card-body">
-                        <fieldset className="fieldset">
+                        <form onSubmit={handleRegister} className="fieldset">
                             <label className="label">Email</label>
-                            <input type="email" className="input" placeholder="Email" />
+                            <input type="email" name="email" className="input" placeholder="Email" />
                             <label className="label">Password</label>
-                            <input type="password" className="input" placeholder="Password" />
+                            <input type="password" name="password" className="input" placeholder="Password" />
                             <div><a className="link link-hover">Already have an account?</a></div>
-                            <button className="btn btn-neutral mt-4">Register Now</button>
-                        </fieldset>
+                            <button type="submit" className="btn btn-neutral mt-4">Register Now</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -27,3 +50,4 @@ const Register = () => {
 };
 
 export default Register;
+
