430ade0cbc5f7903754271cd55d7370745efbfa1
joynalmdabedin <joynalmdabedin440@gmail.com>
2025-07-05 11:41:32 +0600

    add banner motion image

--- src/pages/home/Bannar.jsx
+++ src/pages/home/Bannar.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 
+import { easeInOut, motion } from 'motion/react'
+import team1 from "../../assets/team/team1.jpg"
+import team2 from "../../assets/team/team2.jpg"
 const Bannar = () => {
     return (
         <div className="hero bg-base-200 min-h-96">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className='flex-1'>
-                    <img
-                        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-                        className="max-w-sm rounded-lg shadow-2xl"
+                    <motion.img
+                        animate={{ y: [50, 100, 50] }}
+                        transition={{ duration: 5, delay: 2, repeat: Infinity, ease: easeInOut }}
+                        src={team1}
+                        className="max-w-sm w-64 rounded-t-[50px] rounded-br-[40px] shadow-2xl border-b-12 border-b-blue-600 border-l-12 border-l-blue-600"
+                    />
+                    <motion.img
+                        animate={{ x: [100, 150, 100] }}
+                        transition={{ duration: 5, delay: 2, repeat: Infinity, ease: easeInOut }}
+                        src={team2}
+                        className="max-w-sm w-64 rounded-t-[50px] rounded-br-[40px] shadow-2xl border-b-12 border-b-blue-600 border-l-12 border-l-blue-600"
                     />
                 </div>
                 <div className='flex-1'>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
+                    <motion.h1
+                        animate={{ x: 20 }}
+                        transition={{ duration: 2, delay: 1, ease: easeInOut, repeat: Infinity }}
+                        className="text-5xl font-bold"> <motion.span
+                            animate={{ color: ["#3a34eb", "#6434eb", "#a132ab"] }}
+                            transition={{ duration: 2, delay: 2, repeat: Infinity }}
+                        >Find Job,</motion.span> <br />Hire Creatives</motion.h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        Each month, more than 3 million job seekers turn to website in their search for work,
+                        making over 140,000 applications every single day
                     </p>
                     <button className="btn btn-primary">Get Started</button>
                 </div>
